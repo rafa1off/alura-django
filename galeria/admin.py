52deb1d3a372cae23590b83cc01c311144421d5f
Rafael Giuriatto <rafagiuriatto@hotmail.com>
2023-04-18 23:00:33 -0300

    Aula 3

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Fotografias
 
-# Register your models here.
+class ListandoFotografia(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome', 'legenda')
+
+admin.site.register(Fotografias, ListandoFotografia)
--- galeria/models.py
+++ galeria/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 
 class Fotografias(models.Model):
+
+    categorias = [
+        ('NEBULOSA', 'Nebulosa'),
+        ('GALÁXIA', 'Galáxia'),
+        ('ESTRELA', 'Estrela'),
+        ('PLANETA', 'Planeta')
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=categorias, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
 
