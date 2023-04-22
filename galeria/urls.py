14939ed099234e19bb766f92adaf28af4bbcd20f
Rafael Giuriatto <rafagiuriatto@hotmail.com>
2023-04-22 20:26:36 -0300

    Class Based Views

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from galeria.views import index, imagem, buscar
+from .views import Index, Imagem, buscar
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('imagem/<int:foto_id>', imagem, name='imagem'),
+    path('', Index.as_view(), name='index'),
+    path('imagem/<int:pk>', Imagem.as_view(), name='imagem'),
     path('buscar', buscar, name='buscar')
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from galeria.models import Fotografias
 
 
-def index(request):
+class Index(ListView):
+    model = Fotografias
+    context_object_name = 'cards'
+    template_name = 'galeria/index.html'
+
+
+'''def index(request):
     fotografias = Fotografias.objects.order_by('data').filter(publicada=True)
-    return render(request, 'galeria/index.html', {'cards': fotografias})
+    return render(request, 'galeria/index.html', {'cards': fotografias})'''
 
 
-def imagem(request, foto_id):
+class Imagem(DetailView):
+    model = Fotografias
+    template_name = 'galeria/imagem.html'
+    context_object_name = 'fotografia'
+
+
+'''def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografias, pk=foto_id)
-    return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
+    return render(request, 'galeria/imagem.html', {'fotografia': fotografia})'''
 
 
 def buscar(request):
