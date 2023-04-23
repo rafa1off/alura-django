ca1df247fed27f9b4699f0c96a200c637d12413b
Rafael Giuriatto <rafagiuriatto@hotmail.com>
2023-04-23 15:18:28 -0300

    Forms

---
+++ usuarios/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .forms import LoginForm
+
+
+def login(request):
+    form = LoginForm()
+    return render(request, 'usuarios/login.html', {'form': form})
+
+
+def cadastro(request):
+    return render(request, 'usuarios/cadastro.html')
