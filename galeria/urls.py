5d720269c690032da31c8b2f85583e4683118809
Rafael Giuriatto <rafagiuriatto@hotmail.com>
2023-04-27 18:50:10 -0300

    Class-based Views

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import index, Imagem, buscar
+from .views import Index, Imagem, Buscar
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('imagem/<int:pk>', Imagem.as_view(), name='imagem'),
-    path('buscar', buscar, name='buscar')
+    path('', Index.as_view(), name='index'),
+    path('imagem/<int:foto_id>', Imagem.as_view(), name='imagem'),
+    path('buscar', Buscar.as_view(), name='buscar')
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,32 +1,40 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
+from django.views import View
 from galeria.models import Fotografias
 
 
-'''class Index(ListView):
-    model = Fotografias
-    context_object_name = 'cards'
-    template_name = 'galeria/index.html'''
+class Index(View):
+    def get(self, request):
+        fotografias = Fotografias.objects.order_by('data').filter(publicada=True)
+        return render(request, 'galeria/index.html', {'cards': fotografias})
 
 
-def index(request):
+'''def index(request):
     fotografias = Fotografias.objects.order_by('data').filter(publicada=True)
-    return render(request, 'galeria/index.html', {'cards': fotografias})
+    return render(request, 'galeria/index.html', {'cards': fotografias})'''
 
 
-class Imagem(DetailView):
-    model = Fotografias
-    template_name = 'galeria/imagem.html'
-    context_object_name = 'fotografia'
-
+class Imagem(View):
+    def get(self, request, foto_id):
+        fotografia = get_object_or_404(Fotografias, pk=foto_id)
+        return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
 
 '''def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografias, pk=foto_id)
     return render(request, 'galeria/imagem.html', {'fotografia': fotografia})'''
 
+class Buscar(View):
+    def get(self, request):
+        fotografias = Fotografias.objects.order_by('data').filter(publicada=True)
 
-def buscar(request):
+        if 'buscar' in request.GET:
+            nome_a_buscar = request.GET['buscar']
+            if nome_a_buscar:
+                fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+
+        return render(request, 'galeria/buscar.html', {'cards': fotografias})
+
+'''def buscar(request):
     fotografias = Fotografias.objects.order_by('data').filter(publicada=True)
 
     if 'buscar' in request.GET:
@@ -34,4 +42,4 @@ def buscar(request):
         if nome_a_buscar:
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
-    return render(request, 'galeria/buscar.html', {'cards': fotografias})
+    return render(request, 'galeria/buscar.html', {'cards': fotografias})'''
